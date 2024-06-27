9100a258ca71cac5d670b67a905b6ed5bae5cf6b
shuvo <logic.shuvo@gmail.com>
2024-06-27 19:33:04 +0600

    ongoing

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
 // search bar start
 document.getElementById("search-icon-btn")?.addEventListener("click", () => {
-  document.getElementById("search-container")?.classList.toggle("active")  
-})
+  document.getElementById("search-container")?.classList.toggle("active");
+});
 
 document.getElementById("close-search")?.addEventListener("click", () => {
-  document.getElementById("search-container")?.classList.remove("active")  
-})
+  document.getElementById("search-container")?.classList.remove("active");
+});
 // search bar end
+
+// all slider start
+if (Swiper) {
+  var swiper = new Swiper(".heroSlider", {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: ".heroSlider-next",
+      prevEl: ".heroSlider-prev",
+    },
+  });
+}
+// all slider start end
